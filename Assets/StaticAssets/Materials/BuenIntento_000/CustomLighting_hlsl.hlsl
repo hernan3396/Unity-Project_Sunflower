dd6d7bc1d5534e5549a6635f63de86710a4af840
hernan3396 <hernan3396@gmail.com>
2022-01-05 11:57:40 -0300

    first try at shaders

---
+++ Assets/StaticAssets/Materials/BuenIntento_000/CustomLighting_hlsl.hlsl
@@ -0,0 +1,63 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+struct CustomLightingData {
+    // surface attributes
+    float3 albedo;
+    float smoothness;
+
+    // position and orienta tion
+    float3 normalWS;
+    float3 viewDirectionWS;
+};
+
+float GetSmoothnessPower(float rawSmoothness){
+    return exp2(10 * rawSmoothness + 1);
+}
+
+#ifndef SHADERGRAPH_PREVIEW
+float3 CustomLightHandling(CustomLightingData d, Light light){
+    float3 radiance = light.color;
+
+    float diffuse = saturate(dot(d.normalWS, light.direction));
+    float specularDot = saturate(dot(d.normalWS, normalize(light.direction + d.viewDirectionWS)));
+    float specular = pow(specularDot, GetSmoothnessPower(d.smoothness)) * diffuse;
+
+    float3 color = d.albedo * radiance * (diffuse + specular);
+
+    return color;
+}
+#endif
+
+float3 CalculateCustomLighting(CustomLightingData d){
+#ifdef SHADERGRAPH_PREVIEW
+    // In preview
+    float3 lightDir = float3(0.5,0.5, 0);
+    float intensity = saturate(dot(d.normalWS, lightDir)) + 
+        pow(saturate(dot(d.normalWS, normalize(d.viewDirectionWS + lightDir))), GetSmoothnessPower(d.smoothness));
+    return d.albedo * intensity;
+#else
+    // Get the main light. Located in URP/ShaderLibrary/Lighting/hlsl
+    Light mainLight = GetMainLight();
+
+    float3 color = 0;
+    color+= CustomLightHandling(d, mainLight);
+
+    return color;
+#endif
+}
+
+void CalculateCustomLighting_float(float3 Normal, float3 ViewDirection,
+    float3 Albedo, float Smoothness,
+    out float3 Color) {
+
+        CustomLightingData d;
+        d.normalWS = Normal;
+        d.viewDirectionWS = ViewDirection;
+        d.albedo = Albedo;
+        d.smoothness = Smoothness;
+
+        Color = CalculateCustomLighting(d);
+    }
+
+#endif
