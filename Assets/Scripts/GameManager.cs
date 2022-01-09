c495fabf2b89f1331f690073f050a2b260f7ce9b
hernan3396 <hernan3396@gmail.com>
2022-01-09 13:50:57 -0300

    Almost works!!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        cam.transparencySortMode = TransparencySortMode.CustomAxis;
+        cam.transparencySortAxis = new Vector3(0, 1, 0);
     }
 }
