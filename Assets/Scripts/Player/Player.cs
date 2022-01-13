6504c8c6eaa0a7517277ee79bb9f70e8d04b11c3
hernan3396 <hernan3396@gmail.com>
2022-01-13 11:49:58 -0300

    player detects if it is on light or not

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class Player : MonoBehaviour
@@ -9,17 +10,70 @@ public class Player : MonoBehaviour
     #region Movement
     [SerializeField] private float movementSpeed = 5f;
     #endregion
-    #region Hidden/OnLight
-    // for detecting if player is on a shadow or on a light 
-    private bool isOnLight;
+    #region Shadow/Light
+    private List<Transform> objectives = new List<Transform>();
+    private EnemiesManager enemiesManager;
+    private bool nearLight = false;
     #endregion
-
-    // TODO: hacer la logica de detectar si estas en la luz o no aqui
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    private void Start()
+    {
+        enemiesManager = GameManager.GetInstance.GetEnemiesManager;
+    }
+
+    private void Update()
+    {
+        if (nearLight)
+        {
+            foreach (Transform objective in objectives)
+            {
+                // checks if player is inside a light
+                Debug.DrawRay(transform.position,
+                    objective.position - transform.position, Color.blue);
+
+                RaycastHit2D ray = Physics2D.Raycast(transform.position,
+                    objective.position - transform.position);
+
+                if (ray.collider.CompareTag("LightSource"))
+                {
+                    enemiesManager.PlayerEnterLight();
+                }
+            }
+        }
+    }
+
+    #region Shadow/Light
+    // cant use generic ontrigger script cause
+    // you need other's position
+    // cant use overlap circle because light source's
+    // range may vary
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            nearLight = true;
+            objectives.Add(other.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            objectives.Remove(other.transform);
+            if (objectives.Count < 1)
+            {
+                nearLight = false;
+                enemiesManager.PlayerExitLight();
+            }
+        }
+    }
+    #endregion
 
     #region Getter/Setter
     public Rigidbody2D GetRb
