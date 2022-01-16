6d5e15e5ef02232a31ebd9f45465fe24d4d9b50b
hernan3396 <hernan3396@gmail.com>
2022-01-16 19:08:34 -0300

    added reference to playerStatus
    Change later to a uimanager

--- Assets/Scripts/Managers/EnemiesManager.cs
+++ Assets/Scripts/Managers/EnemiesManager.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class EnemiesManager : MonoBehaviour
 {
     [SerializeField] private bool playerOnLight = false;
+    [SerializeField] private TMP_Text playerStatus;
 
     public void PlayerEnterLight()
     {
         playerOnLight = true;
+        playerStatus.text = "Status: Visible";
     }
 
     public void PlayerExitLight()
     {
         playerOnLight = false;
+        playerStatus.text = "Status: Hidden";
     }
 }
