03da45c8d97baf875520be5c5ae094aab1c8bf07
hernan3396 <hernan3396@gmail.com>
2022-02-02 21:16:47 -0300

    worked on the logic for simple attacks

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -2,10 +2,100 @@ using UnityEngine;
 
 public class Ability : ScriptableObject
 {
+    #region AbilityParameters
+    [Header("Ability Parameters")]
+    [SerializeField] protected LayerMask layer;
     [SerializeField] protected string abilityName;
     [SerializeField] protected float cooldownTime;
-    [SerializeField] protected float activationTime;
     [SerializeField] protected float activeTime;
+    [SerializeField] protected float castTime;
+    [SerializeField] protected int damage;
+    [SerializeField] protected int radius;
+    [Space]
+    #endregion
 
-    public virtual void Activate() { }
+    #region ComboIntento
+    [Header("Combo Intento")]
+    [SerializeField] protected bool isCombo;
+    [SerializeField] protected int comboLength;
+    protected int combo = 0;
+    #endregion
+
+    private void Start()
+    {
+        combo = 0;
+    }
+
+    public virtual void Activate(Vector2 position)
+    {
+        // activar la habilidad
+    }
+
+    #region ComboIntento
+    public void AddCombo()
+    {
+        // es medio al pedo hacerlos para una linea sola
+        // pero esta por si hay que agregar mas logica luego
+        combo += 1;
+    }
+
+    public void ResetCombo()
+    {
+        // es medio al pedo hacerlos para una linea sola
+        // pero esta por si hay que agregar mas logica luego
+        combo = 0;
+    }
+
+    public virtual bool Finisher()
+    {
+        return combo >= comboLength && isCombo ? true : false;
+    }
+    #endregion
+
+    #region Getter/Setter
+    public string AbilityName
+    {
+        get { return abilityName; }
+    }
+
+    public float CooldownTime
+    {
+        get { return cooldownTime; }
+    }
+
+    public float CastTime
+    {
+        get { return castTime; }
+    }
+
+    public float ActiveTime
+    {
+        get { return activeTime; }
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+    }
+
+    public int Radius
+    {
+        get { return radius; }
+    }
+
+    public int ComboLength
+    {
+        get { return comboLength; }
+    }
+
+    public int Combo
+    {
+        get { return combo; }
+    }
+
+    public bool IsCombo
+    {
+        get { return isCombo; }
+    }
+    #endregion
 }
--- Assets/Scripts/Abilities/SimpleAttack.cs
+++ Assets/Scripts/Abilities/SimpleAttack.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 [CreateAssetMenu]
 public class SimpleAttack : Ability
 {
-    public override void Activate()
+    public override void Activate(Vector2 position)
     {
-        Debug.Log("Se activo el: " + abilityName);
+        // Detect enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(position, radius, layer);
+
+        foreach (Collider2D collider2d in hitEnemies)
+        {
+            if (collider2d.CompareTag("Enemy"))
+            {
+                collider2d.GetComponent<Enemy>().TakeDamage(damage);
+            }
+        }
     }
 }
