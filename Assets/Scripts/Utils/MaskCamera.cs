923d824adf28c483f35c86216b6f49ff803c7964
hernan3396 <hernan3396@gmail.com>
2022-03-05 13:51:48 -0300

    fixed weird movement on mask camera

--- Assets/Scripts/Utils/MaskCamera.cs
+++ Assets/Scripts/Utils/MaskCamera.cs
@@ -5,8 +5,14 @@ public class MaskCamera : MonoBehaviour
     [SerializeField] Transform lightsCamera;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = lightsCamera.position;
+        // Late update works best for this
+        // avoids weird object movement perspective (?) dont know what to call it
+        // but it makes sense
+        // change to update and observe the box on game camera and you'll see
+        // transform.position = lightsCamera.position;
+
+        transform.position = new Vector3(lightsCamera.position.x, lightsCamera.position.y, transform.position.z);
     }
 }
